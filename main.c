ac938ba24072a23c0b43785cd0e4aeb8e1eaf551
Ibrahim Rashwan <Hemakress.Gamer@Gmail.com>
2023-10-19 19:59:52 +0300

    added active state to each user using union

--- main.c
+++ main.c
@@ -8,22 +8,43 @@ struct User
 {
     char username[30];
     char password[30];
+
+    union ActiveState
+    {
+        struct inactive
+        {
+            int type;
+        } inactive;
+        
+        struct active
+        {
+            int type;
+        } active;
+    } activeState;
 };
 
 #define MaxUsers 30
 int userCount = 2;
 
 struct User users[MaxUsers] = {
-    {"admin", "admin"},
+    {"admin", "admin" },
     {"username", "password"}
 };
 
 void Login();
 void Register();
-bool Search(int tpye, char* data);
+int Search(int tpye, char* data);
 
 int main()
 {
+    users[0].activeState.active.type = 1;
+    users[1].activeState.inactive.type = 0;
+
+    // printf("user[0].type: %d\n", users[0].activeState.active.type);
+    // printf("user[0].type: %d\n", users[0].activeState.inactive.type);
+
+    // printf("user[1].type: %d\n", users[1].activeState.active.type);
+    // printf("user[1].type: %d\n", users[1].activeState.inactive.type);
 
     char resgisterMsg[18];
     sprintf(resgisterMsg, "Register(%d/%d)", userCount, MaxUsers);
@@ -34,7 +55,8 @@ int main()
         printf("Users:\n");
         for (int i = 0; i < userCount; i++)
         {
-            printf("%d - user: [%s, %s]\n", i, users[i].username, users[i].password);
+            int active = users[i].activeState.active.type == 1;
+            printf("%d - user: [%s, %s, %s]\n", i, users[i].username, users[i].password, active == 1 ? "active" : "inactive");
         }
         printf("\n");
 
@@ -82,16 +104,22 @@ void Login()
         printf("Enter your username: ");
         scanf("%s", username);
 
-        if (!Search(0, username))
+        int index = Search(0, username);
+        if (index == -1)
         {
             printf("Username doesn't exist!\n");
+            continue;
+        }
+
+        if (users[index].activeState.inactive.type == 0) {
+            printf("This user is inactive!\n");
             continue;
         }
 
         printf("Enter your password: ");
         scanf("%s", password);
 
-        if (!Search(1, password))
+        if (strcmp(users[index].password, password) != 0)
         {
             printf("Incorrect password!\n");
             continue;
@@ -108,13 +136,15 @@ void Register()
     char username[30];
     char password[30];
     char passwordConfirmation[30];
+    int active;
 
     while (true)
     {
         printf("Enter your username: ");
         scanf("%s", username);
 
-        if (Search(0, username))
+        int index = Search(0, username);
+        if (index != -1)
         {
             printf("Username already exists!\n");
             continue;
@@ -123,7 +153,7 @@ void Register()
         printf("Enter your password: ");
         scanf("%s", password);
 
-        if (Search(1, password))
+        if (Search(1, password) != -1)
         {
             printf("Password already exists!\n");
             continue;
@@ -138,11 +168,22 @@ void Register()
             continue;
         }
 
+        printf("Mark as active ? answer: (1-yes / 0-no): ");
+        scanf("%d", &active);
+
         break;
     }
 
     strcpy(users[userCount].username, username);
     strcpy(users[userCount].password, password);
+    
+    // Setting the active state.
+    if (active == 1) {
+        users[userCount].activeState.active.type = 1;
+    } else {
+        users[userCount].activeState.inactive.type = 0;
+    }
+
     userCount++;
 
     for (int i = 0; i < userCount; i++)
@@ -153,7 +194,7 @@ void Register()
     printf("Register Successfull! (%s, %s), length: %d\n", users[userCount].username, users[userCount].password, userCount);
 }
 
-bool Search(int type, char *data)
+int Search(int type, char *data)
 {
     for (int i = 0; i < userCount; i++)
     {
@@ -161,14 +202,14 @@ bool Search(int type, char *data)
         {
         case 0:
             if (strcmp(users[i].username, data) == 0)
-                return true;
+                return i;
             break;
         case 1:
             if (strcmp(users[i].password, data) == 0)
-                return true;
+                return i;
             break;
         }
     }
 
-    return false;
+    return -1;
 }
